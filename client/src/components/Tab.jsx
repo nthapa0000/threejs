741a2a865426eda732c08091e6d546ed2bde470a
nthapa0000 <21bma029@nith.ac.in>
2023-07-19 11:43:29 +0530

    We have to add tab features now

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,9 +1,27 @@
 // to switch between different tabs
 import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
 
-const Tab = ({tab,handleClick}) => {
+
+const Tab = ({tab,isFilterTab,isActiveTab,handleClick}) => {
+  const snap = useSnapshot(state);
+  const activeStyle = isFilterTab && isActiveTab 
+  ?{backgroundColor:snap.color,opacity:0.5}
+  :{backgroundColor:'transparent',opacity:1}
   return (
-    <div>Tab</div>
+    <div
+    key={tab.name}
+    className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphishm' : 'rounded-4'}` }
+    onClick={handleClick}
+    style={activeStyle}
+    >
+      <img
+      src={tab.icon}
+      alt={tab.name}
+      className={`${isFilterTab ? 'w-2/3 h-2/3':'w-11/12 h-11/12 object-contain'}`}
+      />
+    </div>
   )
 }
 
